6ede4148c02452a708e20a5226f058d1f3318f70
brian <bchou515@gmail.com>
2019-09-02 14:08:50 -0400

    methods

--- contracts/LinkedList.sol
+++ contracts/LinkedList.sol
@@ -2,9 +2,41 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract LinkedList {
     struct Node {
-        uint256 value;
+        string data;
         uint256 next;
     }
 
     mapping(uint256 => Node) list;
+    Node private head;
+    Node private tail;
+    uint256 private size = 0;
+
+    function appendAtHead(string memory item) public {
+        
+    }
+
+    function appendAtTail(string memory item) public {
+
+    }
+
+    function appendAt(string memory item) public {
+
+    }
+
+    function removeFromHead() public {
+
+    }
+
+    function removeFromTail() public {
+
+    }
+
+    function removeFrom(uint256) public {
+
+    }
+
+    function length() public view returns(uint256) {
+        return size;
+    }
+
 }
